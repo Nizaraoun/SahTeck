74fac421158c136a335f295342309d8e81373087
Nizar aoun <113100482+Nizaraoun@users.noreply.github.com>
2024-02-09 16:57:16 +0100

    first commit

---
+++ SahTech/src/test/java/com/nizar/SahTech/SahTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nizar.SahTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
